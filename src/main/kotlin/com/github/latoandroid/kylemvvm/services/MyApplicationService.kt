4a53e6a521142b9a51e1eff904049709f068a47a
GitHub Action <action@github.com>
2022-03-21 03:29:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/latoandroid/kylemvvm/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/latoandroid/kylemvvm/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.latoandroid.kylemvvm.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.latoandroid.kylemvvm.MyBundle
 
 class MyApplicationService {
 
